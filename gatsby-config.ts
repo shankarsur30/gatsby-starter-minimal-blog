4ed55015da28dba4bf52501d3e1d9d277465d96b
shankarsur30 <141024371+shankarsur30@users.noreply.github.com>
2023-07-31 18:25:27 +0530

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `testtttMinimal Blog`,
+    siteTitle: `Shankar's lil piece of the internet`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
@@ -36,11 +36,11 @@ const config: GatsbyConfig = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/shankarsuresh30`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://www.instagram.com/shankss_30/`,
           },
         ],
       },
