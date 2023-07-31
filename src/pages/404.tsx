d016b847d455210dd4794377c2d7ef365992e2fc
shankarsur30 <141024371+shankarsur30@users.noreply.github.com>
2023-07-31 18:09:54 +0530

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 const NotFound = (_props: PageProps) => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>oops, looks like my content is for my eyes only right now *sigh* </p>
   </Layout>
 )
 
